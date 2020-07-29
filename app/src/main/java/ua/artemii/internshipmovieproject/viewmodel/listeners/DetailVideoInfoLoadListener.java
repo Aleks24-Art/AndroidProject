7565402ebe8ca1458b4c1ca0245ac185112c3b78
Artemii <aleks.artem24@gmial.com>
2020-07-29 12:34:06 +0300

    RxJava + Retrofit + DisposableService

--- app/src/main/java/ua/artemii/internshipmovieproject/viewmodel/listeners/DetailVideoInfoLoadListener.java
+++
@@ -1,8 +0,0 @@
-package ua.artemii.internshipmovieproject.viewmodel.listeners;
-
-import ua.artemii.internshipmovieproject.model.DetailVideoInfoModel;
-
-public interface DetailVideoInfoLoadListener {
-    void detailVideoDataLoad(DetailVideoInfoModel detailVideoInfo);
-    void detailVideoDataFailed(Throwable t);
-}
